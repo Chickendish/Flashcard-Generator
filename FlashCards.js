1eb2af7e1a155d0c0e2b3d9a70619ba79a1cc7bc
Bill Donovan <williamcdonovan@gmail.com>
2017-07-27 10:39:30 -0600

    added flashcards file

--- FlashCards.js
+++ FlashCards.js
@@ -1,2 +1,10 @@
+// npm modules to install
+
 var inquirer = require('inquirer');
 var fs = require('fs');
+var basicCard = require('./BasicCard');
+
+// variable to keep tally of the quiz
+var tally = 0;
+
+fs.readFile()
