b981152757eeb13b30f23d00fd4288444cb20c6d
Bill Donovan <williamcdonovan@gmail.com>
2017-07-28 09:58:00 -0600

    added instructions text

--- BasicCard.js
+++ BasicCard.js
@@ -1,11 +1,16 @@
 function BasicCard(front, back){
 	this.front = front;
 	this.back = back;
-	this.answer = function(answer){
-		if (this.answer === this.back){
-			console.log("Correct!");
-			tally++;
-		}
-	}
+	this.printInfo = function(){
+		console.log("Front: " + this.front + "\nBack: " + this.back);
+	};
+	// this.answer = function(answer){
+	// 	if (this.answer === this.back){
+	// 		console.log("Correct!");
+	// 		tally++;
+	// 	}
+	// }
 
-}
+};
+
+module.exports = BasicCard;
