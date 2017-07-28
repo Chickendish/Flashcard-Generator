cf0ffe6fdca50355c08a37a72642ed0cb9c99151
Bill Donovan <williamcdonovan@gmail.com>
2017-07-28 09:37:44 -0600

    created new array to store flashcards

--- FlashCards.js
+++ FlashCards.js
@@ -4,6 +4,9 @@ var inquirer = require('inquirer');
 var fs = require('fs');
 var basicCard = require('./BasicCard');
 
+// array to store new flashcards
+var flashCards = [];
+
 // variable to keep tally of the quiz
 var tally = 0;
 
@@ -11,4 +14,15 @@ var tally = 0;
 //inquirer prompt questions / answers 
 // to be acquired from a different file
 
-inquirer.prompt()
+inquirer.prompt([
+{
+	name: "front",
+	message: "What is the question?"
+},
+{
+	name: "back",
+	message: "What is the answer?"
+}]).then(function(answers)
+	var newCard = new BasicCard(answers.front, answers.back);
+	flashCards.push(newCard);
+	)
